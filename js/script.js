0de9aeb02493a4ad6eb7f9a7644d250a1ac444c0
hosseinghadirzadeh <hosseinghadirzadeh81@gmail.com>
2022-10-24 04:13:36 +0330

    almost done

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ class shortStory {
         let div = document.createElement('div');
 
         div.innerHTML = `
-                  <div class="box">
+                  <div class="box" onclick="adder(this)">
                     <figure><img src="${this.img}" alt=""></figure>
                     <div>
                         <p id="currencyName">${this.name}</p>
@@ -30,13 +30,15 @@ const options = {
 let oldPrice = 0;
 let array;
 let currencyPrice;
-
+let price;
+let Cname;
 setInterval(() => {
     fetch('https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0', options)
         .then(response => response.json())
         .then(response => {
-            document.getElementsByClassName('name')[0].innerHTML = response.data.coins[0].name;
-            let price = response.data.coins[0].price
+        
+
+            document.getElementsByClassName('name')[0].innerHTML = Cname;
             price = parseInt(price)
             if (oldPrice == 0) {
                 document.getElementsByClassName('priceD')[0].style.color = 'white';
@@ -56,6 +58,8 @@ setInterval(() => {
         .catch(err => console.error(err));
 }, 1000)
 setTimeout(() => {
+        Cname = array.data.coins[0].name;
+            price = array.data.coins[0].price
     array.data.coins.map(item => {
         let obj = new shortStory(item.name, item.price, item.iconUrl);
         obj.Inserter();
@@ -63,21 +67,23 @@ setTimeout(() => {
     currencyPrice = document.querySelectorAll('#currencyPrice');
     // console.log(currencyPrice[0].innerHTML = array.data.coins[1].price)
     setInterval(() => {
-        console.log(currencyPrice.length)
         let i = 0;
         while (i < currencyPrice.length) {
-            currencyPrice[i].innerHTML = array.data.coins[i].price
+            currencyPrice[i].innerHTML = parseInt(array.data.coins[i].price) + '$';
             i++;
         }
-},1000)
+    }, 1000)
 
 }, 1800)
 
-// update(currencyPrice);
-// function update(currencyPrice) {
 
-// }
+function adder(self) {
+    price = self.children[1].children[1].innerHTML;
+    Cname = self.children[1].children[0].innerHTML;
+    document.getElementsByClassName('priceD')[0].innerHTML = price;
+    document.getElementsByClassName('name')[0].innerHTML = Cname;
 
+}
 let date = new Date();
 let day;
 switch (date.getDay()) {
